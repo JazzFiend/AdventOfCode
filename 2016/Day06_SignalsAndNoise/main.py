e78ccb96be27af47d57244e5cab6a321ed149047
Phil Deitz <pdeitz@shutterstock.com>
2017-03-28 09:34:39 -0400

    Day 6 Part 2

--- 2016/Day06_SignalsAndNoise/main.py
+++ 2016/Day06_SignalsAndNoise/main.py
@@ -1,5 +1,4 @@
-
-
+import sys
 
 # Requires a dictionary that has integers as values.  Returns the key(s) with the
 # largest value(s).  In the event of a tie, all keys are returned in alphabetical order.
@@ -15,11 +14,25 @@ def getLargestCount(dictonary):
       keysToReturn.append(key)
   return sorted(keysToReturn)
 
+# Requires a dictionary that has integers as values.  Returns the key(s) with the
+# smallest value(s).  In the event of a tie, all keys are returned in alphabetical order.
+def getSmallestCount(dictonary):
+  keysToReturn = []
+  smallestValue = sys.maxsize
+  for key in dictonary:
+    if(dictonary[key] < smallestValue):
+      smallestValue = dictonary[key]
+      keysToReturn = []
+      keysToReturn.append(key)
+    elif(dictonary[key] == smallestValue):
+      keysToReturn.append(key)
+  return sorted(keysToReturn)
 
 # Start Here
 signalList = [];
 getFileInput = True;
-decodedMessage = ""
+largestDecodedMessage = ""
+smallestDecodedMessage = ""
 
 f = open('input.txt', 'r')
 while(getFileInput):
@@ -37,6 +50,8 @@ for i in range(0, len(signalList[0])):
       charCount[signal[i]] = charCount[signal[i]] + 1
     else:
       charCount[signal[i]] = 1
-  decodedMessage = decodedMessage + getLargestCount(charCount)[0]
+  largestDecodedMessage = largestDecodedMessage + getLargestCount(charCount)[0]
+  smallestDecodedMessage = smallestDecodedMessage + getSmallestCount(charCount)[0]
 
-print ("Decoded Message: %s" % decodedMessage)
+print ("Largest Decoded Message: %s" % largestDecodedMessage)
+print ("Smallest Decoded Message: %s" % smallestDecodedMessage)
