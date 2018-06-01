543199ba978912822d96a453dcbe33054e854dd5
Phil Deitz <pdeitz@shutterstock.com>
2018-06-01 09:19:42 -0400

    Squash Me - Refactor to reduce arguments

--- 2016/Day09_ExplosivesInCyberspace/Decompressor.py
+++ 2016/Day09_ExplosivesInCyberspace/Decompressor.py
@@ -1,78 +1,61 @@
 from DecompressionState import DecompressionState
+from DecompressionStatusFields import DecompressionStatusFields
+from copy import deepcopy
 
 class Decompressor:
   def __init__(self):
-    self.decompressionState = DecompressionState.NOT_COMPRESSED
-    self.charLength = ""
-    self.duplicateLength = ""
+    self._decompressedLength = 0
     return
 
   def calculateUncompressedLengthV1(self, compressedString):
-    self.decompressedLength = 0
-    self.decompressionState = DecompressionState.NOT_COMPRESSED
-    self.charLength = ""
-    self.duplicateLength = ""
+    decompressionStatus = DecompressionStatusFields()
+    self._decompressedLength = 0
     skipCounter = 1
+
     for character in compressedString:
-      if(self.decompressionState == DecompressionState.NOT_COMPRESSED):
-        self.__notCompressedAction(character)
-      elif(self.decompressionState == DecompressionState.EXTRACT_CHAR_LENGTH):
-        self.__extractCharLengthAction(character)
-      elif(self.decompressionState == DecompressionState.EXTRACT_DUPLICATES):
-        self.__extractDuplicatesAction(character)
-      elif(self.decompressionState == DecompressionState.SKIP_DUPLICATES):
-        if(skipCounter < int(self.charLength)):
+      if(decompressionStatus.decompressionState == DecompressionState.NOT_COMPRESSED):
+        decompressionStatus = self.__notCompressedAction(character, decompressionStatus)
+      elif(decompressionStatus.decompressionState == DecompressionState.EXTRACT_CHAR_LENGTH):
+        decompressionStatus = self.__extractCharLengthAction(character, decompressionStatus)
+      elif(decompressionStatus.decompressionState == DecompressionState.EXTRACT_DUPLICATES):
+        decompressionStatus = self.__extractDuplicatesAction(character, decompressionStatus)
+      elif(decompressionStatus.decompressionState == DecompressionState.SKIP_DUPLICATES):
+        if(skipCounter < int(decompressionStatus.charLength)):
           skipCounter += 1
         else:
           skipCounter = 1
           self.charLength = ""
           self.duplicateLength = ""
-          self.decompressionState = DecompressionState.NOT_COMPRESSED
-
-    return self.decompressedLength
+          decompressionStatus.decompressionState = DecompressionState.NOT_COMPRESSED
+    return self._decompressedLength
 
   def calculateUncompressedLengthV2(self, compressedString):
-    self.decompressedLength = 0
-    self.decompressionState = DecompressionState.NOT_COMPRESSED
-    self.charLength = ""
-    self.duplicateLength = ""
-    skipCounter = 1
-    for i in range(0, len(compressedString)):
-      if(self.decompressionState == DecompressionState.NOT_COMPRESSED):
-        self.__notCompressedAction(compressedString[i])
-      elif(self.decompressionState == DecompressionState.EXTRACT_CHAR_LENGTH):
-        self.__extractCharLengthAction(compressedString[i])
-      elif(self.decompressionState == DecompressionState.EXTRACT_DUPLICATES):
-        self.__extractDuplicatesAction(compressedString[i])
-      elif(self.decompressionState == DecompressionState.SKIP_DUPLICATES):
-        if(skipCounter < int(self.charLength)):
-          skipCounter += 1
-        else:
-          skipCounter = 1
-          self.charLength = ""
-          self.duplicateLength = ""
-          self.decompressionState = DecompressionState.NOT_COMPRESSED
+    return self.calculateUncompressedLengthV1(compressedString)
 
-    return self.decompressedLength
-
-  def __notCompressedAction(self, character):
+  def __notCompressedAction(self, character, decompressionStatus):
+    modifiedDecompressionStatus = DecompressionStatusFields.clone(decompressionStatus)
     if(self.__isStartMarker(character)):
-      self.decompressionState = DecompressionState.EXTRACT_CHAR_LENGTH
+      modifiedDecompressionStatus.decompressionState = DecompressionState.EXTRACT_CHAR_LENGTH
     else:
-      self.decompressedLength += 1
+      self._decompressedLength += 1
+    return modifiedDecompressionStatus
 
-  def __extractCharLengthAction(self, character):
+  def __extractCharLengthAction(self, character, decompressionStatus):
+    modifiedDecompressionStatus = DecompressionStatusFields.clone(decompressionStatus)
     if(self.__isMidMarker(character)):
-      self.decompressionState  = DecompressionState.EXTRACT_DUPLICATES
+      modifiedDecompressionStatus.decompressionState = DecompressionState.EXTRACT_DUPLICATES
     else:
-      self.charLength += character
+      modifiedDecompressionStatus.charLength += character
+    return modifiedDecompressionStatus
 
-  def __extractDuplicatesAction(self, character):
+  def __extractDuplicatesAction(self, character, decompressionStatus):
+    modifiedDecompressionStatus = DecompressionStatusFields.clone(decompressionStatus)
     if(self.__isEndMarker(character)):
-      self.decompressionState = DecompressionState.SKIP_DUPLICATES
-      self.decompressedLength += (int(self.charLength) * int(self.duplicateLength))
+      modifiedDecompressionStatus.decompressionState = DecompressionState.SKIP_DUPLICATES
+      self._decompressedLength += (int(modifiedDecompressionStatus.charLength) * int(modifiedDecompressionStatus.duplicateLength))
     else:
-      self.duplicateLength += character
+      modifiedDecompressionStatus.duplicateLength += character
+    return modifiedDecompressionStatus
 
   def __isStartMarker(self, character):
     return character == '('
