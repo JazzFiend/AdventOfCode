9b32c3dc3c5284af6c9f6982c0b375b5b1d26a6e
Philip Deitz <philipd@spotify.com>
2021-05-27 10:29:38 -0400

    Bump rexml on Ruby projects

--- 2020/Day01_ResortRepair/expense_report_analyzer.rb
+++ 2020/Day01_ResortRepair/expense_report_analyzer.rb
@@ -1,25 +1,25 @@
+# frozen_string_literal: true
+
 class ExpenseReportAnalyzer
-  def self.find_sum_two_entries(expenseList, sum)
-    for i in 0..expenseList.length() - 1
-      for j in (i + 1)..expenseList.length() - 1
-        if expenseList[i] + expenseList[j] == sum
-          return [expenseList[i], expenseList[j]]
-        end
+  def self.find_sum_two_entries(expense_list, sum)
+    (0..expense_list.length - 1).each do |i|
+      ((i + 1)..expense_list.length - 1).each do |j|
+        return [expense_list[i], expense_list[j]] if expense_list[i] + expense_list[j] == sum
       end
     end
-    raise "Sum not found"
+    raise 'Sum not found'
   end
 
-  def self.find_sum_three_entries(expenseList, sum)
-    for i in 0..expenseList.length() - 1
-      for j in (i + 1)..expenseList.length() - 1
-        for k in(j + 1)..expenseList.length() - 1
-          if expenseList[i] + expenseList[j] + expenseList[k] == sum
-            return [expenseList[i], expenseList[j], expenseList[k]]
+  def self.find_sum_three_entries(expense_list, sum)
+    (0..expense_list.length - 1).each do |i|
+      ((i + 1)..expense_list.length - 1).each do |j|
+        ((j + 1)..expense_list.length - 1).each do |k|
+          if expense_list[i] + expense_list[j] + expense_list[k] == sum
+            return [expense_list[i], expense_list[j], expense_list[k]]
           end
         end
       end
     end
-    raise "Sum not found"
+    raise 'Sum not found'
   end
 end
--- 2020/Day01_ResortRepair/tc_expense_report_analyzer.rb
+++ 2020/Day01_ResortRepair/tc_expense_report_analyzer.rb
@@ -1,28 +1,30 @@
-require_relative "expense_report_analyzer"
-require "test/unit"
+# frozen_string_literal: true
+
+require_relative 'expense_report_analyzer'
+require 'test/unit'
 
 class TestExpenseReportAnalyzer < Test::Unit::TestCase
   def test_smallest_list
     expenses = [2000, 20]
-    summedEntries = ExpenseReportAnalyzer.find_sum_two_entries(expenses, 2020)
-    assert_equal(summedEntries[0] * summedEntries[1], 40000)
+    summed_entries = ExpenseReportAnalyzer.find_sum_two_entries(expenses, 2020)
+    assert_equal(summed_entries[0] * summed_entries[1], 40_000)
   end
 
   def test_normal_list
     expenses = [675, 979, 366, 299, 1721, 1456]
-    summedEntries = ExpenseReportAnalyzer.find_sum_two_entries(expenses, 2020)
-    assert_equal(summedEntries[0] * summedEntries[1], 514579)
+    summed_entries = ExpenseReportAnalyzer.find_sum_two_entries(expenses, 2020)
+    assert_equal(summed_entries[0] * summed_entries[1], 514_579)
   end
 
-  def test_puzzle_part_1
-    expenses = File.read("input.txt").split.map { |str| str.to_i }
-    summedEntries = ExpenseReportAnalyzer.find_sum_two_entries(expenses, 2020)
-    assert_equal(summedEntries[0] * summedEntries[1], 618144)
+  def test_puzzle_part_one
+    expenses = File.read('input.txt').split.map(&:to_i)
+    summed_entries = ExpenseReportAnalyzer.find_sum_two_entries(expenses, 2020)
+    assert_equal(summed_entries[0] * summed_entries[1], 618_144)
   end
 
-  def test_puzzle_part_2
-    expenses = File.read("input.txt").split.map { |str| str.to_i }
-    summedEntries = ExpenseReportAnalyzer.find_sum_three_entries(expenses, 2020)
-    assert_equal(summedEntries[0] * summedEntries[1] * summedEntries[2], 173538720)
+  def test_puzzle_part_two
+    expenses = File.read('input.txt').split.map(&:to_i)
+    summed_entries = ExpenseReportAnalyzer.find_sum_three_entries(expenses, 2020)
+    assert_equal(summed_entries[0] * summed_entries[1] * summed_entries[2], 173_538_720)
   end
 end
