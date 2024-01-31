d336d4de7074c2a187283d20cb409e923886a2b9
Philip Deitz <philipd@spotify.com>
2024-01-31 18:06:54 -0500

    Support one winning number

--- 2023/day04_scratchcards/src/main/scala/ScratchCardTextParser.scala
+++ 2023/day04_scratchcards/src/main/scala/ScratchCardTextParser.scala
@@ -1,5 +1,12 @@
 object ScratchCardTextParser {
   def parse(text: List[String]): List[(List[Int], List[Int])] = {
-    List.empty
+    if(text.isEmpty) { return List.empty }
+
+    val gameNumbers = text.head.split(":")(1)
+    val winningNumbers = gameNumbers.split('|').head.trim
+    if(winningNumbers.nonEmpty) {
+      return List((List(winningNumbers.toInt), List.empty))
+    }
+    List((List.empty, List.empty))
   }
 }
--- 2023/day04_scratchcards/src/test/scala/ScratchCardTextParserTest.scala
+++ 2023/day04_scratchcards/src/test/scala/ScratchCardTextParserTest.scala
@@ -4,4 +4,15 @@ class ScratchCardTextParserTest extends AnyFunSpec {
   it("No scratch cards should return an empty list") {
     assert(ScratchCardTextParser.parse(List.empty) == List.empty)
   }
+
+  it("A valid scratch card with no numbers should return an empty game") {
+    val gameText = List("Card 3: |")
+    assert(ScratchCardTextParser.parse(gameText) == List((List.empty, List.empty)))
+  }
+
+  it("A game with one winning number") {
+    val gameText = List("Card 3: 12 |")
+    val result = List((List(12), List.empty))
+    assert(ScratchCardTextParser.parse(gameText) == result)
+  }
 }
