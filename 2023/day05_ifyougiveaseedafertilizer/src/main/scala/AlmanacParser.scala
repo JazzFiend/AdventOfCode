d4eda0c89b4bc0a903a57747e200bdfd98af008b
Philip Deitz <philipd@spotify.com>
2024-02-20 13:12:43 -0500

    List of seeds

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacParser.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacParser.scala
@@ -2,9 +2,17 @@ object AlmanacParser {
   def parseSeeds(almanacText: List[String]): List[Int] = {
     if (almanacText.isEmpty) { return List.empty }
 
-    val splitSeeds = almanacText.head.split(":")
+    val splitSeeds = almanacText.head.split(":").toList
     if(splitSeeds.length != 2) { return List.empty }
-    List(splitSeeds.last.trim.toInt)
+
+    extractSeedValues(splitSeeds)
+  }
+
+  private def extractSeedValues(splitSeeds: List[String]): List[Int] = {
+    splitSeeds.last.trim
+      .split(" ")
+      .map(seed => seed.toInt)
+      .toList
   }
 
   def parseMaps(almanacText: List[String]): List[AlmanacMap] = {
--- 2023/day05_ifyougiveaseedafertilizer/src/test/scala/AlmanacParserTest.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/test/scala/AlmanacParserTest.scala
@@ -2,18 +2,25 @@ import org.scalatest.funspec.AnyFunSpec
 
 class AlmanacParserTest extends AnyFunSpec{
   describe("parseSeeds") {
-    it("An empty almanac should give an empty list of seeds") {
-      assert(AlmanacParser.parseSeeds(List.empty) == List.empty)
-    }
+    describe("Error cases") {
+      it("An empty almanac should give an empty list of seeds") {
+        assert(AlmanacParser.parseSeeds(List.empty) == List.empty)
+      }
 
-    it("An almanac with no seeds listed should give an empty list of seeds") {
-      val noSeeds = List("seeds:")
-      assert(AlmanacParser.parseSeeds(noSeeds) == List.empty)
+      it("An almanac with no seeds listed should give an empty list of seeds") {
+        val noSeeds = List("seeds:")
+        assert(AlmanacParser.parseSeeds(noSeeds) == List.empty)
+      }
     }
 
     it("One seed") {
       val oneSeed = List("seeds: 68")
       assert(AlmanacParser.parseSeeds(oneSeed) == List(68))
+    }
+
+    it("Many seeds") {
+      val manySeeds = List("seeds: 16 1 384 34")
+      assert(AlmanacParser.parseSeeds(manySeeds) == List(16, 1, 384, 34))
     }
   }
 
