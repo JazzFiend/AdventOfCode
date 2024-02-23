5a06a35ec98cbb1c98498b46f327d3c37ec9cf6b
Philip Deitz <philipd@spotify.com>
2024-02-23 15:55:55 -0500

    Override equals for AlmanacMap

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacMap.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacMap.scala
@@ -1,3 +1,12 @@
 class AlmanacMap(val source: String, val destination: String, val mapRanges: List[MapRange]) {
-
+  override def equals(that: Any): Boolean = {
+    that match {
+      case that: AlmanacMap =>
+        that.isInstanceOf[AlmanacMap] &&
+        that.source == source &&
+        that.destination == destination &&
+        that.mapRanges.equals(mapRanges)
+      case _ => false
+    }
+  }
 }
--- 2023/day05_ifyougiveaseedafertilizer/src/test/scala/AlmanacMapTest.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/test/scala/AlmanacMapTest.scala
@@ -1,3 +1,31 @@
-class AlmanacMapTest {
+import org.scalatest.funspec.AnyFunSpec
 
+class AlmanacMapTest extends AnyFunSpec {
+  describe("Equals") {
+    it("Two identical AlmanacMaps should be equal") {
+      val one = AlmanacMap("source", "destination", List(MapRange(3, 2, 1)))
+      val two = AlmanacMap("source", "destination", List(MapRange(3, 2, 1)))
+      assert(one == two)
+    }
+  }
+
+  describe("Not equals") {
+    it("Different source") {
+      val one = AlmanacMap("source", "destination", List(MapRange(3, 2, 1)))
+      val two = AlmanacMap("different", "destination", List(MapRange(3, 2, 1)))
+      assert(one != two)
+    }
+
+    it("Different destination") {
+      val one = AlmanacMap("source", "destination", List(MapRange(3, 2, 1)))
+      val two = AlmanacMap("source", "different", List(MapRange(3, 2, 1)))
+      assert(one != two)
+    }
+
+    it("Different mapRanges") {
+      val one = AlmanacMap("source", "destination", List(MapRange(3, 2, 1)))
+      val two = AlmanacMap("source", "destination", List.empty)
+      assert(one != two)
+    }
+  }
 }
