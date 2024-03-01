ccff2a8cc7ea9cdc2f23e67f175bdc4feba7afe4
Philip Deitz <philipd@spotify.com>
2024-03-01 15:40:05 -0500

    Almanac Pipeline - One map

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacPipeline.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacPipeline.scala
@@ -1,5 +1,7 @@
 object AlmanacPipeline {
-  def processPipeline(seeds: List[Int]): List[Int] = {
-    seeds
+  def processPipeline(seeds: List[Int], maps: List[AlmanacMap]): List[Int] = {
+    if(maps.isEmpty) { return seeds }
+
+    maps.head.mapSourceValues(seeds)
   }
 }
--- 2023/day05_ifyougiveaseedafertilizer/src/test/scala/AlmanacPipelineTest.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/test/scala/AlmanacPipelineTest.scala
@@ -2,7 +2,13 @@ import org.scalatest.funspec.AnyFunSpec
 
 class AlmanacPipelineTest extends AnyFunSpec{
   it("Seeds with no maps should just be a pass through") {
-    val almanac = List(34, 58, 1, 334)
-    assert(AlmanacPipeline.processPipeline(almanac) == List(34, 58, 1, 334))
+    val seeds = List(34, 58, 1, 334)
+    assert(AlmanacPipeline.processPipeline(seeds, List.empty) == List(34, 58, 1, 334))
+  }
+
+  it("Seeds and one map") {
+    val seeds = List(1, 2, 3, 4)
+    val maps = List(AlmanacMap("a", "b", List(MapRange(100, 2, 2))))
+    assert(AlmanacPipeline.processPipeline(seeds, maps) == List(1, 100, 101, 4))
   }
 }
