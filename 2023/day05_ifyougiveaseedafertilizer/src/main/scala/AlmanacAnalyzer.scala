0d4a1d2ea5b11da913aaf8723b0331163e2371f0
Philip Deitz <philipd@spotify.com>
2024-03-05 15:46:17 -0500

    Replace old AlmanacMap and complete Part 1

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacAnalyzer.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacAnalyzer.scala
@@ -6,18 +6,3 @@ object AlmanacAnalyzer {
     finalValues.min
   }
 }
-
-// First we need to parse the input strings. This consists of two functions.
-// (DONE) The first function is extracting the desired seeds. This is just a list of ints.
-// (DONE) Second, we need the maps. This should result in a list of AlmanacMap objects.
-// An AlmanacMap should take in the source name, destination name, and a list of MapRanges
-// A MapRange should take in a destination range start, a source range start, and a range length.
-
-// (DONE) Next we take the AlmanacMap class and start performing the mapping. Once the values in the constructor are passed
-// we can set up the map. Then, we make a function that takes a list of ints and returns the corresponding list of mapped value.
-
-// (DONE) Finally, we set up the pipeline. Maybe this is its own class? Don't know yet.
-// We specify the starting ingredient ("seed"). Then perform the map with the desired values. This should give us the mapped list and the mapped ingredient.
-// Keep repeating this until the current source ingredient doesn't exist in the map list. That implies that we've reached the end. Then do the last calculation (in this case, minimum value) and return it.
-
-// Okay, this is all working, but its taking forever and we're blowing the heap space. Let's start by making an Almanac Map Test with one of the values from the puzzle.
--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacMap.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/AlmanacMap.scala
@@ -1,37 +1,20 @@
 class AlmanacMap(val source: String, val destination: String, val mapRanges: List[MapRange]) {
   def mapSourceValues(numbersToMap: List[Long]):List[Long] = {
-    val mappings = computeMappings
     numbersToMap.map(inputNumber => {
-      val allMapRangeResults = mapAgainstAllRanges(mappings, inputNumber)
-      calculateMapResult(inputNumber, allMapRangeResults)
-    })
-  }
-
-  private def computeMappings: List[List[Long]] = {
-    mapRanges.map(mapRange => {
-      (mapRange.sourceRangeStart until mapRange.sourceRangeStart + mapRange.rangeLength).toList
-    })
-  }
-
-  private def mapAgainstAllRanges(mappings: List[List[Long]], inputNumber: Long): List[Long] = {
-    mappings.zipWithIndex.map((mapping, index) => {
-      if (mapping.contains(inputNumber)) {
-        val offset = mapping.indexOf(inputNumber)
-        mapRanges(index).destinationRangeStart + offset
-      } else {
+      val mapIndexToUse = mapRanges.map(mapRange => mapRange.isInRange(inputNumber))
+        .zipWithIndex.filter((value, index) => value)
+        .map((value, index) => index)
+      if (mapIndexToUse.isEmpty) {
         inputNumber
+      } else {
+        calculateMappedValue(inputNumber, mapIndexToUse.head)
       }
     })
   }
 
-  private def calculateMapResult(inputNumber: Long, allMapRangeResults: List[Long]) = {
-    allMapRangeResults.fold(inputNumber)((prev, next) => {
-      if (next != inputNumber) {
-        next
-      } else {
-        prev
-      }
-    })
+  private def calculateMappedValue(inputNumber: Long, mapRangeIndex: Int): Long = {
+    val rangeToUse = mapRanges(mapRangeIndex)
+    rangeToUse.destinationRangeStart + (inputNumber - rangeToUse.sourceRangeStart)
   }
 
   override def equals(that: Any): Boolean = {
