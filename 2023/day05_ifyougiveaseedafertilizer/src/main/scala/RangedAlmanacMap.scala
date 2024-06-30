e9207140f19194ff5446146c2b8fed0bc6d68662
Philip Deitz <philipd@spotify.com>
2024-06-29 22:05:41 -0400

    RangedAlmanacMap - Map range with no overlap

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/RangedAlmanacMap.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/RangedAlmanacMap.scala
@@ -1,3 +1,5 @@
 class RangedAlmanacMap (val source: String, val destination: String, val mapRanges: List[MapRange]) {
-
+  def mapSourceValues(rangesToMap: List[(Long, Long)]):List[(Long, Long)] = {
+    rangesToMap
+  }
 }
--- 2023/day05_ifyougiveaseedafertilizer/src/test/scala/RangedAlmanacMapTest.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/test/scala/RangedAlmanacMapTest.scala
@@ -1,9 +1,11 @@
 import org.scalatest.funspec.AnyFunSpec
 
 class RangedAlmanacMapTest extends AnyFunSpec {
-  it("Constructor Test") {
-    RangedAlmanacMap("source", "destination", List(new MapRange(10, 20, 5)))
+  describe("mapSourceValues") {
+    it("No Overlap") {
+      val almanacMap = new RangedAlmanacMap("source", "destination", List(new MapRange(10, 20, 5)))
+      val sourceRanges = List((40L, 50L))
+      assert(almanacMap.mapSourceValues(sourceRanges) == sourceRanges)
+    }
   }
-
-
 }
