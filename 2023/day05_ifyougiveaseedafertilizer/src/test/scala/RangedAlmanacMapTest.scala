e90341a2295166fdb734bdcef9dd6c5574d8cb55
Philip Deitz <philipd@spotify.com>
2024-06-29 22:11:16 -0400

    Write plan for MapRange mods

--- 2023/day05_ifyougiveaseedafertilizer/src/test/scala/RangedAlmanacMapTest.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/test/scala/RangedAlmanacMapTest.scala
@@ -7,5 +7,14 @@ class RangedAlmanacMapTest extends AnyFunSpec {
       val sourceRanges = List((40L, 50L))
       assert(almanacMap.mapSourceValues(sourceRanges) == sourceRanges)
     }
+
+// Need to modify MapRange class first. It needs to take in a range of numbers and output the numbers that are in range.
+
+//    it("Overlap one number at beginning") {
+//      val almanacMap = new RangedAlmanacMap("source", "destination", List(new MapRange(10, 20, 5)))
+//      val sourceRanges = List((15L, 20L))
+//      val expected = List((15L, 19L), (20L, 20L))
+//      assert(almanacMap.mapSourceValues(sourceRanges) == expected)
+//    }
   }
 }
