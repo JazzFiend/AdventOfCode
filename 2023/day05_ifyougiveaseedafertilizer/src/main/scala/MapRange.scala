ad09aa0da9ea24084cc1106f558f28bd4ae91785
Philip Deitz <philipd@spotify.com>
2024-07-01 22:11:59 -0400

    Greatly simplified findRangeOverlap

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/MapRange.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/MapRange.scala
@@ -1,34 +1,33 @@
 class MapRange(val destinationRangeStart: Long, val sourceRangeStart: Long, val rangeLength: Long) {
+  private def sourceRangeEnd = {
+    sourceRangeStart + rangeLength - 1
+  }
+
   def isInRange(number: Long): Boolean = {
     number >= sourceRangeStart && number <= sourceRangeEnd
   }
 
   def findRangeOverlap(inputRange: (Long, Long)): Option[(Long, Long)] = {
-    if(inputRange._2 < sourceRangeStart || inputRange._1 > sourceRangeEnd) {
+    if (noOverlap(inputRange)) {
       return None
-    } else if(inputRange._1 <= sourceRangeStart && inputRange._2 >= sourceRangeEnd) {
-     return Some(sourceRangeStart, sourceRangeEnd)
-    } else if(inputRange._2 >= sourceRangeStart && inputRange._1 <= sourceRangeStart) {
-      return Some((sourceRangeStart, inputRange._2))
-    } else if(inputRange._1 > sourceRangeStart && inputRange._2 < sourceRangeEnd) {
-      return Some(inputRange)
-    } else if(inputRange._1 <= sourceRangeEnd) {
-      return Some(inputRange._1, sourceRangeEnd)
     }
-    None
+
+    val low = Math.max(inputRange._1, sourceRangeStart)
+    val high = Math.min(sourceRangeEnd, inputRange._2)
+    Some(low, high)
   }
 
-  private def sourceRangeEnd = {
-    sourceRangeStart + rangeLength - 1
+  private def noOverlap(inputRange: (Long, Long)) = {
+    inputRange._2 < sourceRangeStart || inputRange._1 > sourceRangeEnd
   }
 
   override def equals(that: Any): Boolean = {
     that match {
       case that: MapRange =>
         that.isInstanceOf[MapRange] &&
-        that.destinationRangeStart == destinationRangeStart &&
-        that.sourceRangeStart == sourceRangeStart &&
-        that.rangeLength == rangeLength
+          that.destinationRangeStart == destinationRangeStart &&
+          that.sourceRangeStart == sourceRangeStart &&
+          that.rangeLength == rangeLength
       case _ => false
     }
   }
