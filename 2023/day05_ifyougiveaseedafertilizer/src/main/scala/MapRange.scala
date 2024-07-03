030118241da1fa7a5b5e6579f1ef1d506a3a2696
Philip Deitz <philipd@spotify.com>
2024-07-03 16:59:30 -0400

    MapRange - Refactor computeMap

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/MapRange.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/MapRange.scala
@@ -22,22 +22,27 @@ class MapRange(val destinationRangeStart: Long, val sourceRangeStart: Long, val 
   def rangeMap(range: (Long, Long)): List[(Long, Long)] = {
     val overlap = findRangeOverlap(range)
     if (overlap.isEmpty) { return List(range) }
+    
     computeMap(range, overlap.get)
   }
 
-  // **** REFACTOR STEP *****
   private def computeMap(range: (Long, Long), overlap: (Long, Long)): List[(Long, Long)] = {
     if(range == overlap) {
       List((destinationRangeStart, destinationRangeStart + rangeLength - 1))
-    } else if(range._2 >= overlap._1 && range._1 < overlap._1) {
+    } else if(overlapLeft(range, overlap)) {
       val noChange = (range._1, overlap._1 - 1)
       val change = (destinationRangeStart, destinationRangeStart + computeRangeLength(overlap))
       List(noChange, change)
     } else {
-      val change = (destinationRangeStart + rangeLength - computeRangeLength(overlap) - 1, destinationRangeStart + rangeLength - 1)
-      val noChange = (range._1 + computeRangeLength(overlap) + 1, range._2)
+      val changeLength = rangeLength - computeRangeLength(overlap) - 1
+      val change = (destinationRangeStart + changeLength, destinationRangeStart + rangeLength - 1)
+      val noChange = (overlap._2 + 1, range._2)
       List(change, noChange)
     }
+  }
+
+  private def overlapLeft(range: (Long, Long), overlap: (Long, Long)): Boolean = {
+    range._2 >= overlap._1 && range._1 < overlap._1
   }
 
   private def computeRangeLength(range: (Long, Long)): Integer = {
