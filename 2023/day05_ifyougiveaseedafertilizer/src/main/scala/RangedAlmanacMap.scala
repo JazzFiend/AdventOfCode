d9ee1c25f17c3a7819add2e3efddf3f789b8fd42
Philip Deitz <philipd@spotify.com>
2024-07-04 15:20:31 -0400

    RangedAlmanacMap - Map all values given

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/RangedAlmanacMap.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/RangedAlmanacMap.scala
@@ -1,5 +1,7 @@
 class RangedAlmanacMap (val source: String, val destination: String, val mapRanges: List[MapRange]) {
   def mapSourceValues(rangesToMap: List[(Long, Long)]): List[(Long, Long)] = {
-    rangesToMap
+    val mapRange = mapRanges.head
+    val range = rangesToMap.head
+    mapRange.rangeMap(range)
   }
 }
--- 2023/day05_ifyougiveaseedafertilizer/src/test/scala/RangedAlmanacMapTest.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/test/scala/RangedAlmanacMapTest.scala
@@ -7,5 +7,11 @@ class RangedAlmanacMapTest extends AnyFunSpec {
       val sourceRanges = List((50L, 60L))
       assert(almanacMap.mapSourceValues(sourceRanges) == sourceRanges)
     }
+
+    it("Map all values given") {
+      val almanacMap = RangedAlmanacMap("source", "destination", List(new MapRange(100, 10, 10)))
+      val sourceRanges = List((10L, 19L))
+      assert(almanacMap.mapSourceValues(sourceRanges) == List((100L, 109L)))
+    }
   }
 }
