28706630fc7c560afbf291294f2afa25a919185a
Philip Deitz <philipd@spotify.com>
2024-07-04 13:36:55 -0400

    RangedAlmanacMap - No source values to map

--- 2023/day05_ifyougiveaseedafertilizer/src/main/scala/RangedAlmanacMap.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/main/scala/RangedAlmanacMap.scala
@@ -1,21 +1,5 @@
 class RangedAlmanacMap (val source: String, val destination: String, val mapRanges: List[MapRange]) {
   def mapSourceValues(rangesToMap: List[(Long, Long)]): List[(Long, Long)] = {
-    val mapRange = mapRanges.head
-    val range = rangesToMap.head
-    val overlapOption = mapRange.findRangeOverlap(range)
-    if (overlapOption.isEmpty) { return rangesToMap }
-
-    performMap(mapRange, range, overlapOption.get)
-  }
-
-  // *** REFACTOR: Should this live in the MapRange class?
-  // Probably. Keeps destinationRange self contained, and lets this class deal solely with
-  // collecting and deduping the ranges
-  // Current plan: Leave this for now but start writing this function in MapRange. Omce that's done,
-  // come back to this class and remove this.
-  private def performMap(mapRange: MapRange, range: (Long, Long), overlap: (Long, Long)) = {
-    val noChange = (range._1, overlap._1 - 1)
-    val changed = (mapRange.destinationRangeStart, mapRange.destinationRangeStart)
-    List(noChange, changed)
+    rangesToMap
   }
 }
--- 2023/day05_ifyougiveaseedafertilizer/src/test/scala/RangedAlmanacMapTest.scala
+++ 2023/day05_ifyougiveaseedafertilizer/src/test/scala/RangedAlmanacMapTest.scala
@@ -2,18 +2,10 @@ import org.scalatest.funspec.AnyFunSpec
 
 class RangedAlmanacMapTest extends AnyFunSpec {
   describe("mapSourceValues") {
-    it("No Overlap - too small") {
-      val almanacMap = new RangedAlmanacMap("source", "destination", List(new MapRange(200, 100, 10)))
-      val sourceRanges = List((0L, 50L))
+    it("No source values to map") {
+      val almanacMap = RangedAlmanacMap("source", "destination", List(new MapRange(100, 10, 10)))
+      val sourceRanges = List((50L, 60L))
       assert(almanacMap.mapSourceValues(sourceRanges) == sourceRanges)
-    }
-
-    // **** Should we return a List or a Set? ****
-    it("Overlap one number at beginning") {
-      val almanacMap = new RangedAlmanacMap("source", "destination", List(new MapRange(200, 100, 10)))
-      val sourceRanges = List((90L, 100L))
-      val expected = List((90L, 99L), (200L, 200L))
-      assert(almanacMap.mapSourceValues(sourceRanges) == expected)
     }
   }
 }
