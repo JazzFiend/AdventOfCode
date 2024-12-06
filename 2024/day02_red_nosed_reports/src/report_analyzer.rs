94c66d06d6cb794974ad2937588eecc0a9800622
Philip Deitz <philipd@spotify.com>
2024-12-06 17:38:40 -0500

    Safe reports - No reports

--- 2024/day02_red_nosed_reports/src/report_analyzer.rs
+++ 2024/day02_red_nosed_reports/src/report_analyzer.rs
@@ -1,10 +1,17 @@
 pub fn count_safe_reports(reports: Vec<&str>) -> i32 {
+    if reports.is_empty() { return 0 }
     2
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn no_reports() {
+        assert_eq!(count_safe_reports(vec![]), 0);
+    }
+
 
     #[test]
     fn acceptance() {
