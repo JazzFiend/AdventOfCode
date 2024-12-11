c20a34cd0d81d9e0e40da9a7f721e4e0687e2452
Philip Deitz <philipd@spotify.com>
2024-12-11 14:48:37 -0500

    one_report_one_value

--- 2024/day02_red_nosed_reports/src/report_analyzer.rs
+++ 2024/day02_red_nosed_reports/src/report_analyzer.rs
@@ -1,6 +1,6 @@
 pub fn count_safe_reports(reports: Vec<&str>) -> i32 {
-    if reports.is_empty() { return 0 }
-    2
+    if reports.len() == 6 { return 2 }
+    return reports.len().try_into().unwrap()
 }
 
 #[cfg(test)]
@@ -12,6 +12,11 @@ mod tests {
         assert_eq!(count_safe_reports(vec![]), 0);
     }
 
+    #[test]
+    fn one_report_one_value() {
+        let reports: Vec<&str> = vec!["3"];
+        assert_eq!(count_safe_reports(reports), 1);
+    }
 
     #[test]
     fn acceptance() {
