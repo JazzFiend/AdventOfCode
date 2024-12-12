3697eb82468bb731f84e6635789aa01c8047fe26
Philip Deitz <philipd@spotify.com>
2024-12-12 06:12:59 -0500

    Finished Part One

--- 2024/day02_red_nosed_reports/src/report_analyzer.rs
+++ 2024/day02_red_nosed_reports/src/report_analyzer.rs
@@ -1,16 +1,11 @@
 pub fn count_safe_reports(reports: Vec<&str>) -> i32 {
-    if reports.len() == 0 {
-        return 0;
-    }
-    if reports.len() == 6 {
-        return 2;
-    }
+    if reports.len() == 0 { return 0; }
 
     return reports
         .iter()
         .map(|report| convert_string_to_ints(&report))
         .filter(|report_numbers| {
-            all_decreasing(report_numbers.to_vec()) || all_increasing(report_numbers.to_vec())
+            is_safe(report_numbers.to_vec())
         })
         .count()
         .try_into()
@@ -24,16 +19,30 @@ fn convert_string_to_ints(vector: &str) -> Vec<i32> {
         .collect()
 }
 
+fn is_safe(report: Vec<i32>) -> bool {
+    return all_decreasing(report.clone()) || all_increasing(report.clone());
+}
+
 fn all_increasing(vector: Vec<i32>) -> bool {
-    vector.iter().zip(vector.iter().skip(1)).all(|(a, b)| a < b)
+    vector
+        .iter()
+        .zip(vector.iter().skip(1))
+        .all(|(a, b)| (a < b) && (b - a <= 3))
 }
 
 fn all_decreasing(vector: Vec<i32>) -> bool {
-    vector.iter().zip(vector.iter().skip(1)).all(|(a, b)| a > b)
+    vector
+        .iter()
+        .zip(vector.iter().skip(1))
+        .all(|(a, b)| (a > b) && (a - b <= 3))
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs::File;
+    use std::io::{self, BufRead, BufReader};
+    use std::path::Path;
+
     use super::*;
 
     #[test]
@@ -80,19 +89,23 @@ mod tests {
                 let reports: Vec<&str> = vec!["5 4 6 3 7 2 8 1 9 0 10"];
                 assert_eq!(count_safe_reports(reports), 0);
             }
-
-            // #[test]
-            // fn more_than_three() {
-            //     let reports: Vec<&str> = vec!["0 1 3 6 10", "20 19 18 1"];
-            //     assert_eq!(count_safe_reports(reports), 0);
-            // }
         }
     }
 
-    #[test]
-    fn multiple_reports_safe() {
-        let reports: Vec<&str> = vec!["10 9 8", "0 2 4 6 8"];
-        assert_eq!(count_safe_reports(reports), 2);
+    mod many_reports {
+        use super::*;
+
+        #[test]
+        fn multiple_reports_safe() {
+            let reports: Vec<&str> = vec!["10 9 8", "0 2 4 6 8"];
+            assert_eq!(count_safe_reports(reports), 2);
+        }
+
+        #[test]
+        fn unsafe_more_than_three() {
+            let reports: Vec<&str> = vec!["0 1 3 6 10", "20 19 18 1"];
+            assert_eq!(count_safe_reports(reports), 0);
+        }
     }
 
     #[test]
@@ -107,5 +120,29 @@ mod tests {
         ];
 
         assert_eq!(count_safe_reports(reports), 2);
+    }
+
+    #[test]
+    fn puzzle_part_one() {
+        let filename = "./src/input.txt";
+        match read_lines(filename) {
+            Ok(lines) => {
+                let reports = lines.iter().map(|l| l.as_str()).collect();
+                assert_eq!(count_safe_reports(reports), 279)
+            }
+            Err(e) => {
+                println!("Error reading file: {}", e);
+                assert!(false, "An error occured")
+            }
+        }
+    }
+
+    fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(filename)?;
+        let reader = BufReader::new(file);
+        reader.lines().collect()
     }
 }
