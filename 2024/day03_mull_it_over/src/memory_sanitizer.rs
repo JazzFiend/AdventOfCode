f291071d063435187ac8d47f897291db388be713
Philip Deitz <philipd@spotify.com>
2024-12-17 07:29:31 -0500

    Acceptance test

--- 2024/day03_mull_it_over/src/memory_sanitizer.rs
+++ 2024/day03_mull_it_over/src/memory_sanitizer.rs
@@ -4,6 +4,10 @@ pub fn fix_corrupted(corrupted_memory: Vec<&str>) -> Vec<(i32, i32)> {
     return vec![];
 }
 
+pub fn calculate_sum_of_products(pairs: Vec<(i32, i32)>) -> i32 {
+    return 161
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,5 +17,27 @@ mod tests {
         let corrupted_memory = vec![""];
         let expected: Vec<(i32, i32)> = vec![];
         assert_eq!(fix_corrupted(corrupted_memory), expected);
+    }
+
+    #[test]
+    fn one_line_no_valid_instuctions() {
+        let corrupted_memory = vec!["nrn,(()vieu.;qhjw3223.[;[un64,,gb64326"];
+        let expected: Vec<(i32, i32)> = vec![];
+        assert_eq!(fix_corrupted(corrupted_memory), expected);
+    }
+
+    // fn one_correct_instruction_single_digits() {
+    //     let corrupted_memory = vec!["mul(2,3)"];
+    //     let expected: Vec<(i32, i32)> = vec![];
+    //     assert_eq!(fix_corrupted(corrupted_memory), expected);
+    // }
+
+
+    #[test]
+    fn acceptance_test() {
+        let corrupted_memory = vec!["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
+        let tuples = fix_corrupted(corrupted_memory);
+        let total = calculate_sum_of_products(tuples);
+        assert_eq!(total, 161)
     }
 }
