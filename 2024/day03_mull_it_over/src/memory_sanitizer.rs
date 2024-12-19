ecf58ddba28ca811cb617b1abb1530ec4a62a548
Philip Deitz <philipd@spotify.com>
2024-12-18 21:12:49 -0500

    sum of products

--- 2024/day03_mull_it_over/src/memory_sanitizer.rs
+++ 2024/day03_mull_it_over/src/memory_sanitizer.rs
@@ -6,81 +6,99 @@ pub fn fix_corrupted(corrupted_memory: Vec<&str>) -> Vec<(i32, i32)> {
     return corrupted_memory
         .iter()
         .flat_map(|line| {
-            multiply_regex
-                .captures_iter(line)
-                .map(|command| {
-                    (
-                        command[1].parse::<i32>().unwrap(),
-                        command[2].parse::<i32>().unwrap(),
-                    )
-                })
+            multiply_regex.captures_iter(line).map(|command| {
+                (
+                    command[1].parse::<i32>().unwrap(),
+                    command[2].parse::<i32>().unwrap(),
+                )
+            })
         })
         .collect();
 }
 
 pub fn calculate_sum_of_products(pairs: Vec<(i32, i32)>) -> i32 {
-    return 161;
+    return pairs
+        .iter()
+        .map(|pair| pair.0 * pair.1)
+        .reduce(|a, b| a + b)
+        .unwrap();
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn empty_memory() {
-        let corrupted_memory = vec![];
-        let expected: Vec<(i32, i32)> = vec![];
-        assert_eq!(fix_corrupted(corrupted_memory), expected);
-    }
-
-    mod one_line {
+    mod fix_corrupted {
         use super::*;
 
         #[test]
-        fn blank_memory() {
-            let corrupted_memory = vec![""];
+        fn empty_memory() {
+            let corrupted_memory = vec![];
             let expected: Vec<(i32, i32)> = vec![];
             assert_eq!(fix_corrupted(corrupted_memory), expected);
         }
 
-        #[test]
-        fn no_valid_instuctions() {
-            let corrupted_memory = vec!["nrn,(()vieu.;qhjw3223.[;[un64,,gb64326"];
-            let expected: Vec<(i32, i32)> = vec![];
-            assert_eq!(fix_corrupted(corrupted_memory), expected);
-        }
-
-        mod one_correct_instruction {
+        mod one_line {
             use super::*;
 
             #[test]
-            fn single_digits() {
-                let corrupted_memory = vec!["mul(2,3)"];
-                let expected: Vec<(i32, i32)> = vec![(2, 3)];
+            fn blank_memory() {
+                let corrupted_memory = vec![""];
+                let expected: Vec<(i32, i32)> = vec![];
                 assert_eq!(fix_corrupted(corrupted_memory), expected);
             }
 
             #[test]
-            fn multi_digits() {
-                let corrupted_memory = vec!["mul(47,589)"];
-                let expected: Vec<(i32, i32)> = vec![(47, 589)];
+            fn no_valid_instuctions() {
+                let corrupted_memory = vec!["nrn,(()vieu.;qhjw3223.[;[un64,,gb64326"];
+                let expected: Vec<(i32, i32)> = vec![];
+                assert_eq!(fix_corrupted(corrupted_memory), expected);
+            }
+
+            mod one_correct_instruction {
+                use super::*;
+
+                #[test]
+                fn single_digits() {
+                    let corrupted_memory = vec!["mul(2,3)"];
+                    let expected: Vec<(i32, i32)> = vec![(2, 3)];
+                    assert_eq!(fix_corrupted(corrupted_memory), expected);
+                }
+
+                #[test]
+                fn multi_digits() {
+                    let corrupted_memory = vec!["mul(47,589)"];
+                    let expected: Vec<(i32, i32)> = vec![(47, 589)];
+                    assert_eq!(fix_corrupted(corrupted_memory), expected);
+                }
+            }
+
+            #[test]
+            fn multiple_correct_instructions() {
+                let corrupted_memory = vec!["qqewdrmul(2,29)hgyuiymul(55,5)njjnrjrei"];
+                let expected: Vec<(i32, i32)> = vec![(2, 29), (55, 5)];
                 assert_eq!(fix_corrupted(corrupted_memory), expected);
             }
         }
 
         #[test]
-        fn multiple_correct_instructions() {
-            let corrupted_memory = vec!["qqewdrmul(2,29)hgyuiymul(55,5)njjnrjrei"];
-            let expected: Vec<(i32, i32)> = vec![(2, 29), (55, 5)];
+        fn multiple_lines() {
+            let corrupted_memory = vec!["mul(2,3)", "mul)(mul(2,44)(5,9)mul(345)"];
+            let expected: Vec<(i32, i32)> = vec![(2, 3), (2, 44)];
             assert_eq!(fix_corrupted(corrupted_memory), expected);
         }
     }
 
-    #[test]
-    fn multiple_lines() {
-        let corrupted_memory = vec!["mul(2,3)", "mul)(mul(2,44)(5,9)mul(345)"];
-        let expected: Vec<(i32, i32)> = vec![(2, 3), (2, 44)];
-        assert_eq!(fix_corrupted(corrupted_memory), expected);
+    mod calculate_sum_of_products {
+        use super::*;
+
+        #[test]
+        fn sum_of_products() {
+            assert_eq!(
+                calculate_sum_of_products(vec![(2, 5), (10, 20), (1, 0), (11, 234)]),
+                (2 * 5) + (10 * 20) + (1 * 0) + (11 * 234)
+            )
+        }
     }
 
     #[test]
