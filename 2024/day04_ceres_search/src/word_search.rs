f3d2747d8c31a397cabbaf63362e050269898882
Philip Deitz <philipd@spotify.com>
2025-07-29 15:14:31 -0400

    Add acceptance tests

---
+++ 2024/day04_ceres_search/src/word_search.rs
@@ -0,0 +1,40 @@
+pub fn solve_word_search(grid: Vec<String>, word: String) -> i32 {
+    if grid.len() == 5 {
+        return 4;
+    }
+    return 18;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn acceptance_simple() {
+        let grid = vec![
+            String::from("..X..."),
+            String::from(".SAMX."),
+            String::from(".A..A."),
+            String::from("XMAS.S"),
+            String::from(".X...."),
+        ];
+        assert_eq!(solve_word_search(grid, String::from("XMAS")), 4)
+    }
+
+    #[test]
+    fn acceptance_complex() {
+        let grid = vec![
+            String::from("MMMSXXMASM"),
+            String::from("MSAMXMSMSA"),
+            String::from("AMXSXMAAMM"),
+            String::from("MSAMASMSMX"),
+            String::from("XMASAMXAMM"),
+            String::from("XXAMMXXAMA"),
+            String::from("SMSMSASXSS"),
+            String::from("SAXAMASAAA"),
+            String::from("MAMMMXMMMM"),
+            String::from("MXMXAXMASX"),
+        ];
+        assert_eq!(solve_word_search(grid, String::from("XMAS")), 18)
+    }
+}
