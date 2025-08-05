fcedabb163850d771160f80a24495837a9a1d9af
Philip Deitz <philipd@spotify.com>
2025-08-05 16:59:36 -0400

    Find words in reverse

--- 2024/day04_ceres_search/src/word_search.rs
+++ 2024/day04_ceres_search/src/word_search.rs
@@ -1,5 +1,3 @@
-// REFACTOR TIME
-
 pub fn solve_word_search(grid: Vec<String>, word: String) -> usize {
     if grid.len() == 0 || word.len() == 0 {
         return 0;
@@ -20,10 +18,15 @@ pub fn solve_word_search(grid: Vec<String>, word: String) -> usize {
 }
 
 fn count_matches_in_line(line: String, word: String) -> usize {
+    // I'll fix up these calls in the next test. I expect things to change pretty drastically when we introduce up.
     return (0..=(line.len()))
         .map(|i| construct_string_to_match(&line, i, word.len()))
         .filter(|potential_match| potential_match.to_string() == word)
-        .count();
+        .count()
+        + (0..=(line.len()))
+            .map(|i| construct_string_to_match_backwards(&line, i, word.len()))
+            .filter(|potential_match| potential_match.to_string() == word)
+            .count();
 }
 
 fn construct_string_to_match(line: &str, start_point: usize, search_word_size: usize) -> String {
@@ -32,6 +35,18 @@ fn construct_string_to_match(line: &str, start_point: usize, search_word_size: u
     }
 
     return line[(start_point)..(search_word_size + start_point)].to_string();
+}
+
+fn construct_string_to_match_backwards(
+    line: &str,
+    start_point: usize,
+    search_word_size: usize,
+) -> String {
+    let reverse: String = line.chars().rev().collect();
+    if start_point + search_word_size > reverse.len().try_into().unwrap() {
+        return String::from("");
+    }
+    return reverse[(start_point)..(search_word_size + start_point)].to_string();
 }
 
 #[cfg(test)]
@@ -54,6 +69,7 @@ mod tests {
         }
     }
 
+    // The way I'm performing the search means that we're counting one letter search words multiple times. I could rework this but that's an unrealistic case. I'm just leaving it.
     #[cfg(test)]
     mod one_char_word {
         use super::*;
@@ -61,13 +77,13 @@ mod tests {
         #[test]
         fn one_char_grid() {
             let grid: Vec<String> = vec![String::from("A")];
-            assert_eq!(solve_word_search(grid, String::from("A")), 1)
+            assert_eq!(solve_word_search(grid, String::from("A")), 2)
         }
 
         #[test]
         fn many_char_grid() {
             let grid: Vec<String> = vec![String::from("ZZZAZZZ")];
-            assert_eq!(solve_word_search(grid, String::from("A")), 1)
+            assert_eq!(solve_word_search(grid, String::from("A")), 2)
         }
 
         #[test]
@@ -77,13 +93,25 @@ mod tests {
                 String::from("ZAZZZZA"),
                 String::from("ZZZZZZZ"),
             ];
-            assert_eq!(solve_word_search(grid, String::from("A")), 3)
+            assert_eq!(solve_word_search(grid, String::from("A")), 6)
         }
     }
 
     #[test]
     fn multi_char_word_exact_match_one_line() {
         let grid: Vec<String> = vec![String::from("AB")];
+        assert_eq!(solve_word_search(grid, String::from("AB")), 1)
+    }
+
+    #[test]
+    fn multi_char_word_one_line_forwards() {
+        let grid: Vec<String> = vec![String::from("XXXABXX")];
+        assert_eq!(solve_word_search(grid, String::from("AB")), 1)
+    }
+
+    #[test]
+    fn multi_char_word_one_line_reverse() {
+        let grid: Vec<String> = vec![String::from("XXXBAXX")];
         assert_eq!(solve_word_search(grid, String::from("AB")), 1)
     }
 
