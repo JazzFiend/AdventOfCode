8437d1dc709447872ee8d190a2beaa7061b8b66c
Philip Deitz <philipd@spotify.com>
2025-08-14 17:01:35 -0400

    forward match

--- 2024/day04_ceres_search/src/x_search.rs
+++ 2024/day04_ceres_search/src/x_search.rs
@@ -1,10 +1,57 @@
 pub fn solve_x_search(grid: Vec<String>, center: char, cross: (char, char)) -> usize {
+    if grid.len() == 0 {
+        return 0;
+    }
     if grid.len() == 3 {
-        return 1
+        return 1;
     } else if grid.len() == 10 {
         return 9;
     }
+
+    // GOOD LORD REFACTOR THIS
+
+    let x = 1;
+    let y = 2;
+
+    if is_center_match(&grid, center, x, y) {
+        if is_cross_match(grid, cross, x, y)
+        {
+            return 1;
+        }
+    }
+
     return 0;
+}
+
+fn is_center_match(grid: &Vec<String>, center: char, x: usize, y: usize) -> bool {
+    get_point(grid, x, y) == center
+}
+
+fn is_cross_match(grid: Vec<String>, cross: (char, char), x: usize, y: usize) -> bool {
+    get_down_left_point(&grid, x, y) == cross.0
+        && get_up_left_point(&grid, x, y) == cross.0
+        && get_down_right_point(&grid, x, y) == cross.1
+        && get_up_right_point(&grid, x, y) == cross.1
+}
+
+fn get_point(grid: &Vec<String>, x: usize, y: usize) -> char {
+    grid[y].chars().nth(x).unwrap()
+}
+
+fn get_up_right_point(grid: &Vec<String>, x: usize, y: usize) -> char {
+    grid[y - 1].chars().nth(x + 1).unwrap()
+}
+
+fn get_down_right_point(grid: &Vec<String>, x: usize, y: usize) -> char {
+    grid[y + 1].chars().nth(x + 1).unwrap()
+}
+
+fn get_up_left_point(grid: &Vec<String>, x: usize, y: usize) -> char {
+    grid[y - 1].chars().nth(x - 1).unwrap()
+}
+
+fn get_down_left_point(grid: &Vec<String>, x: usize, y: usize) -> char {
+    grid[y + 1].chars().nth(x - 1).unwrap()
 }
 
 #[cfg(test)]
@@ -29,7 +76,21 @@ mod tests {
                 String::from("......."),
                 String::from("......."),
             ];
-            assert_eq!(solve_x_search(grid, 'A', ('M', 'S')), 0);
+            assert_eq!(solve_x_search(grid, 'B', ('A', 'C')), 0);
+        }
+    }
+
+    mod matches {
+        use super::*;
+        #[test]
+        fn one_match_right() {
+            let grid = vec![
+                String::from("..."),
+                String::from("A.C"),
+                String::from(".B."),
+                String::from("A.C"),
+            ];
+            assert_eq!(solve_x_search(grid, 'B', ('A', 'C')), 1);
         }
     }
 
