b9526809435a6843c149c7559eec43b27413bc73
Philip Deitz <philipd@spotify.com>
2025-08-14 19:35:29 -0400

    other combos of matches

--- 2024/day04_ceres_search/src/x_search.rs
+++ 2024/day04_ceres_search/src/x_search.rs
@@ -8,18 +8,14 @@ pub fn solve_x_search(grid: Vec<String>, center: char, cross: (char, char)) -> u
         return 9;
     }
 
-    // GOOD LORD REFACTOR THIS
-
     let x = 1;
     let y = 2;
 
     if is_center_match(&grid, center, x, y) {
-        if is_cross_match(grid, cross, x, y)
-        {
+        if is_cross_match(&grid, cross, x, y) {
             return 1;
         }
     }
-
     return 0;
 }
 
@@ -27,11 +23,20 @@ fn is_center_match(grid: &Vec<String>, center: char, x: usize, y: usize) -> bool
     get_point(grid, x, y) == center
 }
 
-fn is_cross_match(grid: Vec<String>, cross: (char, char), x: usize, y: usize) -> bool {
-    get_down_left_point(&grid, x, y) == cross.0
-        && get_up_left_point(&grid, x, y) == cross.0
-        && get_down_right_point(&grid, x, y) == cross.1
-        && get_up_right_point(&grid, x, y) == cross.1
+fn is_cross_match(grid: &Vec<String>, cross: (char, char), x: usize, y: usize) -> bool {
+    is_match_negative_slope(grid, cross, x, y) && is_match_positive_slope(grid, cross, x, y)
+}
+
+fn is_match_positive_slope(grid: &Vec<String>, cross: (char, char), x: usize, y: usize) -> bool {
+    (get_down_left_point(&grid, x, y) == cross.0 && get_up_right_point(&grid, x, y) == cross.1)
+        || (get_down_left_point(&grid, x, y) == cross.1
+            && get_up_right_point(&grid, x, y) == cross.0)
+}
+
+fn is_match_negative_slope(grid: &Vec<String>, cross: (char, char), x: usize, y: usize) -> bool {
+    (get_up_left_point(&grid, x, y) == cross.0 && get_down_right_point(&grid, x, y) == cross.1)
+        || (get_up_left_point(&grid, x, y) == cross.1
+            && get_down_right_point(&grid, x, y) == cross.0)
 }
 
 fn get_point(grid: &Vec<String>, x: usize, y: usize) -> char {
@@ -82,6 +87,7 @@ mod tests {
 
     mod matches {
         use super::*;
+
         #[test]
         fn one_match_right() {
             let grid = vec![
@@ -89,6 +95,39 @@ mod tests {
                 String::from("A.C"),
                 String::from(".B."),
                 String::from("A.C"),
+            ];
+            assert_eq!(solve_x_search(grid, 'B', ('A', 'C')), 1);
+        }
+
+        #[test]
+        fn one_match_left() {
+            let grid = vec![
+                String::from("..."),
+                String::from("C.A"),
+                String::from(".B."),
+                String::from("C.A"),
+            ];
+            assert_eq!(solve_x_search(grid, 'B', ('A', 'C')), 1);
+        }
+
+        #[test]
+        fn one_match_combo_1() {
+            let grid = vec![
+                String::from("..."),
+                String::from("A.A"),
+                String::from(".B."),
+                String::from("C.C"),
+            ];
+            assert_eq!(solve_x_search(grid, 'B', ('A', 'C')), 1);
+        }
+
+        #[test]
+        fn one_match_combo_2() {
+            let grid = vec![
+                String::from("..."),
+                String::from("C.C"),
+                String::from(".B."),
+                String::from("A.A"),
             ];
             assert_eq!(solve_x_search(grid, 'B', ('A', 'C')), 1);
         }
