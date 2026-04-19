0d1699557b51cb493da60b9fee9c0a3cbc4a2782
Philip Deitz <philipd@spotify.com>
2026-04-19 19:33:45 -0400

    Acceptance test for combination

--- 2025/src/Day01SecretEntrance/CombinationLock.hs
+++ 2025/src/Day01SecretEntrance/CombinationLock.hs
@@ -1,7 +1,12 @@
 module Day01SecretEntrance.CombinationLock where
 
-runCombinationFinalNumber :: String -> Int
-runCombinationFinalNumber "" = 50
-runCombinationFinalNumber ('R' : distance) = (50 + read distance) `mod` 100
-runCombinationFinalNumber ('L' : distance) = (50 - read distance) `mod` 100
-runCombinationFinalNumber badInput = error ("Invalid direction: " ++ badInput ++ ". Expected an L or R.")
+runCombinationFinalNumber :: [String] -> Int
+runCombinationFinalNumber = foldl applyMove 50
+  where
+    applyMove current "" = current
+    applyMove current ('R' : distance) = (current + read distance) `mod` 100
+    applyMove current ('L' : distance) = (current - read distance) `mod` 100
+    applyMove _ badInput = error ("Invalid direction: " ++ badInput ++ ". Expected an L or R.")
+
+runCombinationCountNumber :: [String] -> Int -> Int
+runCombinationCountNumber _ _ = 0
--- 2025/test/Day01SecretEntrance/CombinationLockSpec.hs
+++ 2025/test/Day01SecretEntrance/CombinationLockSpec.hs
@@ -6,26 +6,51 @@ import Test.Hspec
 
 spec :: Spec
 spec = describe "CombinationLock" $ do
-  context "when given an empty input" $ do
-    it "the counter should not move" $
-      runCombinationFinalNumber "" `shouldBe` 50
+  describe "runCombinationFinalNumber" $ do
+    context "when given an empty input" $ do
+      it "the counter should not move" $
+        runCombinationFinalNumber [""] `shouldBe` 50
 
-  context "when given a right move" $ do
-    it "the counter should move right" $ do
-      runCombinationFinalNumber "R20" `shouldBe` 70
+    context "when given a right move" $ do
+      it "the counter should move right" $ do
+        runCombinationFinalNumber ["R20"] `shouldBe` 70
 
-  context "when given a left move" $ do
-    it "the counter should move left" $ do
-      runCombinationFinalNumber "L20" `shouldBe` 30
+    context "when given a left move" $ do
+      it "the counter should move left" $ do
+        runCombinationFinalNumber ["L20"] `shouldBe` 30
 
-  context "when given an invalid move" $ do
-    it "throws an error" $ do
-      evaluate (runCombinationFinalNumber "U20") `shouldThrow` anyErrorCall
+    context "when given an invalid move" $ do
+      it "throws an error" $ do
+        evaluate (runCombinationFinalNumber ["U20"]) `shouldThrow` anyErrorCall
 
-  context "when moving right and overflowing the dial" $ do
-    it "should wrap around" $ do
-      runCombinationFinalNumber "R50" `shouldBe` 0
+    context "when moving right and overflowing the dial" $ do
+      it "should wrap around" $ do
+        runCombinationFinalNumber ["R50"] `shouldBe` 0
 
-  context "when moving left and overflowing the dial" $ do
-    it "should wrap around" $ do
-      runCombinationFinalNumber "L60" `shouldBe` 90
+    context "when moving left and overflowing the dial" $ do
+      it "should wrap around" $ do
+        runCombinationFinalNumber ["L60"] `shouldBe` 90
+
+    context "when given multiple moves" $ do
+      it "should calculate the final number" $ do
+        runCombinationFinalNumber ["R20", "L10"] `shouldBe` 60
+
+    context "acceptance tests" $ do
+      it "example 1: should calculate the final number" $ do
+        runCombinationFinalNumber
+          [ "L68",
+            "L30",
+            "R48",
+            "L5",
+            "R60",
+            "L55",
+            "L1",
+            "L99",
+            "R14",
+            "L82"
+          ]
+          `shouldBe` 32
+  describe "runCombinationCountNumber" $ do
+    context "when given an empty input and tracking number is not the default" $ do
+      it "the final count is 0" $ do
+        runCombinationCountNumber [""] 0 `shouldBe` 0
