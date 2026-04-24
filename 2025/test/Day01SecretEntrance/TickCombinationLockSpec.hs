2d36034344a65463da4004f294ac11dd6d6364ba
Philip Deitz <philipd@spotify.com>
2026-04-24 17:08:00 -0400

    Move the combo lock to the tracking number but don't pass it yet.

--- 2025/test/Day01SecretEntrance/TickCombinationLockSpec.hs
+++ 2025/test/Day01SecretEntrance/TickCombinationLockSpec.hs
@@ -13,3 +13,11 @@ spec = describe "TickCombinationLock" $ do
     context "when given an empty input and tracking number is the default" $ do
       it "the final count is 1" $ do
         countAllTicks [] 50 `shouldBe` 1
+
+    context "when we never pass the tracking number" $ do
+      it "the final count is 0" $ do
+        countAllTicks ["R10", "L20", "R30", "R20"] 0 `shouldBe` 0
+
+    context "when we hit the tracking number" $ do
+      it "the final count should be equal to the number of times we land on the tracking number (but never pass it)" $ do
+        countAllTicks ["R50", "R50", "R50", "L100"] 0 `shouldBe` 3
